a5e97d167a7866d062d0b962f04482dd7fc5e2e7
MOURIG ABDELLAH <mourig.abdellah.dev@gmail.com>
2023-06-24 20:04:17 +0100

    Change the welcome message

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import random
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def get_random_coordinates():
     ip_address = request.json['ip']
 
