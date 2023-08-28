95cfbfbc90074d104cdeaf91981511aa3a1650e1
MOURIG ABDELLAH <mourig.abdellah.dev@gmail.com>
2023-08-28 11:01:46 +0200

    adding timezone

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import random
 app = Flask(__name__)
 CORS(app)
 
-def generate_random_coordinates(region):
+def generate_random_coordinates(region, timezone):
     geolocator = Nominatim(user_agent="my-app")
     location = geolocator.geocode(region)
     if location is None:
@@ -22,7 +22,8 @@ def generate_random_coordinates(region):
 
     result = {
         'latitude': random_lat,
-        'longitude': random_lon
+        'longitude': random_lon,
+        'timezone' : timezone
     }
 
     return jsonify(result), 200
@@ -34,14 +35,17 @@ def get_random_coordinates():
         return jsonify({'error': 'Failed to retrieve ip adresse.'}), 400
     # Get IP region
     ip_info_url = f"https://ipapi.co/{ip_address}/region/"
+    ip_timezone_url = f"https://ipapi.co/{ip_address}/timezone/"
     response = requests.get(ip_info_url)
+    response_timezone = requests.get(ip_timezone_url)
     if response.status_code == 200:
         region = response.text.strip()
+        timezone = response_timezone.text.strip()
     else:
         return jsonify({'error': 'Failed to retrieve region of the IP.'}), 400
 
     # Call the generate_random_coordinates function
-    result = generate_random_coordinates(region)
+    result = generate_random_coordinates(region, timezone)
 
     return result
 
